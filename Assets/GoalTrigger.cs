983f5f3e865872194f6c1a756a330dbedcb6e784
nasirpi <pi.nasir@mail.utoronto.ca>
2021-01-23 12:22:31 -0500

    add spawning and moving players

---
+++ Assets/GoalTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalTrigger : MonoBehaviour
+{
+
+    public int playersReached = 0;
+    public int playersFailed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playersReached + playersFailed == 10)
+        {
+            Debug.Log("RESULTS: " + playersReached + "/10");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        {
+            other.gameObject.SetActive(false);
+            playersReached += 1;
+        }
+    }
+
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 2.0f;
+    public Vector3 direction = Vector3.forward;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += speed * Time.deltaTime * direction;
+        if (transform.position.y < -10.0f)
+        {
+            LosePlayer();
+        }
+    }
+
+    private void LosePlayer()
+    {
+        GameObject.FindGameObjectWithTag("goal").GetComponent<GoalTrigger>().playersFailed += 1;
+        gameObject.SetActive(false);
+    }
+}
