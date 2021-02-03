d7a164554fc329745cf4307215f51a21dfbf68d8
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-03 15:08:18 -0500

    Setting up master scene

--- Assets/Scripts/BuildManager.cs
+++ Assets/Scripts/BuildManager.cs
@@ -1,34 +1,22 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
-
-        private int CompareRaycastHit(RaycastHit a, RaycastHit b) 
-        { 
-            if (a.point.y > b.point.y)
-            {
-                return -1;
-            } else if (a.point.y < b.point.y)
-            {
-                return 1;
-            } else 
-            {
-                return 0;
-            } 
-        }
-
-
     public static BuildManager instance;
 
     public static bool isBuilding = false;
 
     public Transform currBuilding;
 
-    public GameObject Wall;
-    public GameObject Arrow;
+    // 0 - Wall
+    // 1 - Arrow
+    public GameObject[] BuildingPrefabs;
+
+    private GameObject Wall;
+    private GameObject Arrow;
+    private MoneyManager moneyManager;
 
     private void Awake()
     {
@@ -37,65 +25,73 @@ public class BuildManager : MonoBehaviour
             Destroy(instance);
         }
         instance = this;
-    }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
+        Wall = BuildingPrefabs[0];
+        Arrow = BuildingPrefabs[1];
+        moneyManager = GameObject.FindGameObjectWithTag("moneyManager").GetComponent<MoneyManager>();
     }
 
     public void BuildWall()
     {
         GameObject newBuilding = Instantiate(Wall, transform.position, Quaternion.identity) as GameObject;
+
         newBuilding.transform.parent = transform;
+        newBuilding.transform.position = transform.position + newBuilding.transform.localScale.y / 2 * newBuilding.transform.up;
+
         currBuilding = newBuilding.transform;
+        moneyManager.PurchaseItem(MoneyManager.ITEM_WALL);
     }
 
     public void BuildArrow()
     {
         GameObject newBuilding = Instantiate(Arrow, transform.position, Quaternion.identity) as GameObject;
-        newBuilding.transform.parent = transform;
-        currBuilding = newBuilding.transform;
-    }
 
+        newBuilding.transform.parent = transform;
+        newBuilding.transform.position = transform.position + newBuilding.transform.localScale.y / 2 * newBuilding.transform.up;
+
+        currBuilding = newBuilding.transform;
+        moneyManager.PurchaseItem(MoneyManager.ITEM_ARROW);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit[] hits = MouseRaycast();
-
-        Array.Sort(hits, CompareRaycastHit);
-
-        if (hits.Length > 0)
+        RaycastHit hit;
+        if (MouseRaycast("Foundation", out hit))
         {
-            foreach (RaycastHit hit in hits)
+            if (currBuilding != null)
             {
-                if (hit.transform.CompareTag("Foundation") && currBuilding != null)
-                {
-                    currBuilding.transform.position = hit.point + hit.transform.up * 0.1f;
-                    break;
-                }
+                Debug.Log(hit.point + currBuilding.localScale.y / 2 * currBuilding.up);
+                currBuilding.position = hit.point + currBuilding.localScale.y / 2 * currBuilding.up;
+            }
+        }
 
-                if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (MouseRaycast("Building", out hit))
+            {
+                if (currBuilding != null)
                 {
-                    if (hit.transform.CompareTag("Building"))
+                    if (hit.transform == currBuilding)
                     {
-                        if (currBuilding != null)
-                        {
-                            currBuilding.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(0.78f, 0.0f, 0.0f, 1.0f));
-                            currBuilding = null;
-                        }
-                        else
-                        {
-                            currBuilding = hit.transform;
-                            currBuilding.GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(0.78f, 0.0f, 0.0f, 0.5f));
-                            
-                        }
+                        currBuilding.GetComponent<Building>().setColorPlaced();
+                        currBuilding = null;
                     }
+                    else
+                    {
+                        currBuilding.GetComponent<Building>().setColorPlaced();
+                        currBuilding = hit.transform;
+                        currBuilding.GetComponent<Building>().setColorSelected();
+                    }
+                }
+                else
+                {
+                    currBuilding = hit.transform;
+                    currBuilding.GetComponent<Building>().setColorSelected();
                 }
             }
         }
+
 
         if (Input.GetKey(KeyCode.E))
         {
@@ -103,7 +99,8 @@ public class BuildManager : MonoBehaviour
             {
                 currBuilding.RotateAround(transform.position, transform.up, Time.deltaTime * 90f);
             }
-        } else if (Input.GetKey(KeyCode.Q))
+        }
+        else if (Input.GetKey(KeyCode.Q))
         {
             if (currBuilding != null)
             {
@@ -112,12 +109,11 @@ public class BuildManager : MonoBehaviour
         }
     }
 
-    private RaycastHit[] MouseRaycast()
+    private bool MouseRaycast(string targetLayerName, out RaycastHit hit)
     {
+        int layerMask = 1 << LayerMask.NameToLayer(targetLayerName);
+
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        return Physics.RaycastAll(ray);
+        return Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask);
     }
-
-    
-
 }
