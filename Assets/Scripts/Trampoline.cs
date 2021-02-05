dae0e6a880a5d692018d6804a65aeffa5fe8fc96
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-05 15:02:40 -0500

    zero y velocity on trampoline entry

--- Assets/Scripts/Trampoline.cs
+++ Assets/Scripts/Trampoline.cs
@@ -19,6 +19,8 @@ public class Trampoline : Building
     private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("player"))
         {
+            Vector3 currVelocity = other.gameObject.GetComponent<Rigidbody>().velocity;
+            other.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(currVelocity.x, 0, currVelocity.z);
             Vector3 jumpDirection = (260*Vector3.up);
             other.gameObject.GetComponent<Rigidbody>().AddRelativeForce(jumpDirection);
         }
