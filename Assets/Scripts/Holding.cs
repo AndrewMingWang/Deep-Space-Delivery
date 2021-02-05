5bf1fb0f0647e853d8f7c91bf07616cb544e5580
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-05 15:53:25 -0500

    Add text UI to holding building

--- Assets/Scripts/Holding.cs
+++ Assets/Scripts/Holding.cs
@@ -1,11 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Holding : Building
 {
-    public float scaleThreshold = 0.11f;
+    // How much does a player entering the hitbox change the scale?
+    public const float EntryScaleDelta = 0.05f;
+
+    public int ThresholdNumHeldPlayers = 4;
+
     public List<GameObject> HeldPlayers = new List<GameObject>();
+
+    public TMP_Text ThresholdText;
+
+    public void IncrementThreshold()
+    {
+        ThresholdNumHeldPlayers += 1;
+
+        Vector3 currScale = transform.localScale;
+        transform.localScale = new Vector3(currScale.x, EntryScaleDelta * ThresholdNumHeldPlayers, currScale.z);
+        Vector3 currPosition = transform.localPosition;
+        transform.localPosition = new Vector3(currPosition.x, currPosition.y - currScale.y / 2 + transform.localScale.y / 2, currPosition.z);
+
+        ThresholdText.text = HeldPlayers.Count.ToString() + "/" + ThresholdNumHeldPlayers.ToString();
+    }
+
+    public void DecrementThreshold()
+    {
+        if (ThresholdNumHeldPlayers == 1)
+        {
+            return;
+        }
+        ThresholdNumHeldPlayers -= 1;
+
+        Vector3 currScale = transform.localScale;
+        transform.localScale = new Vector3(currScale.x, EntryScaleDelta * ThresholdNumHeldPlayers, currScale.z);
+        Vector3 currPosition = transform.localPosition;
+        transform.localPosition = new Vector3(currPosition.x, currPosition.y - currScale.y / 2 + transform.localScale.y / 2, currPosition.z);
+
+        ThresholdText.text = HeldPlayers.Count.ToString() + "/" + ThresholdNumHeldPlayers.ToString();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,12 +50,12 @@ public class Holding : Building
             HeldPlayers.Add(other.gameObject);
 
             Vector3 currScale = transform.localScale;
-            transform.localScale = Vector3.Max(currScale - new Vector3(0, 0.05f, 0), Vector3.zero);
+            transform.localScale = Vector3.Max(currScale - new Vector3(0, EntryScaleDelta, 0), Vector3.zero);
             Vector3 currPosition = transform.localPosition;
             transform.localPosition = new Vector3(currPosition.x, currPosition.y - currScale.y / 2 + transform.localScale.y / 2, currPosition.z);
         }
 
-        if (transform.localScale.y < scaleThreshold)
+        if (HeldPlayers.Count > ThresholdNumHeldPlayers)
         {
             foreach (GameObject player in HeldPlayers)
             {
@@ -29,6 +64,8 @@ public class Holding : Building
             HeldPlayers.Clear();
             gameObject.SetActive(false);
         }
+
+        ThresholdText.text = HeldPlayers.Count.ToString() + "/" + ThresholdNumHeldPlayers.ToString();
     }
 
     private void OnTriggerExit(Collider other)
@@ -39,9 +76,11 @@ public class Holding : Building
             HeldPlayers.Remove(other.gameObject);
 
             Vector3 currScale = transform.localScale;
-            transform.localScale = currScale + new Vector3(0, 0.05f, 0);
+            transform.localScale = currScale + new Vector3(0, EntryScaleDelta, 0);
             Vector3 currPosition = transform.localPosition;
             transform.localPosition = new Vector3(currPosition.x, currPosition.y - currScale.y / 2 + transform.localScale.y / 2, currPosition.z);
+
+            ThresholdText.text = HeldPlayers.Count.ToString() + "/" + ThresholdNumHeldPlayers.ToString();
         }
     }
 }
