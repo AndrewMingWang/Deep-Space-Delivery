90a91743b5b7246f1c21bb03e8ecff1b42a6f209
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-13 18:46:49 -0500

    make camera move in 22.5 degree increments and speed up movement

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,8 +3,8 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float sensitivity = 0.05f;
-    public float rotationDuration = 0.5f;
+    public float Sensitivity = 0.05f;
+    public float RotationDuration = 0.2f;
     private float _rotationElapsed;
 
     Vector3 cameraPos;
@@ -35,8 +35,8 @@ public class CameraMovement : MonoBehaviour
 
     public void Panning()
     {
-        cameraPos -= transform.right * (Input.GetAxis("Horizontal") * -1) * sensitivity;
-        cameraPos -= transform.up * (Input.GetAxis("Vertical") * -1) * sensitivity;
+        cameraPos -= transform.right * (Input.GetAxis("Horizontal") * -1) * Sensitivity;
+        cameraPos -= transform.up * (Input.GetAxis("Vertical") * -1) * Sensitivity;
         transform.position = cameraPos;
     }
 
@@ -45,17 +45,17 @@ public class CameraMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
         {
             Camera.main.orthographicSize = 3;
-            sensitivity = 0.02f;
+            Sensitivity = 0.02f;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
         {
             Camera.main.orthographicSize = 5;
-            sensitivity = 0.05f;
+            Sensitivity = 0.05f;
         }
         else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
         {
             Camera.main.orthographicSize = 8;
-            sensitivity = 0.05f;
+            Sensitivity = 0.05f;
         }
     }
 
@@ -69,23 +69,23 @@ public class CameraMovement : MonoBehaviour
                 _rotateToObj.transform.position = new Vector3(SceneObjects.transform.position.x, SceneObjects.transform.position.y, SceneObjects.transform.position.z);
                 _rotateToObj.transform.rotation = new Quaternion(_rotateFrom.x, _rotateFrom.y, _rotateFrom.z, _rotateFrom.w);
                 if (Input.GetKeyDown(KeyCode.Q)){
-                    _rotateToObj.transform.Rotate(0,-45, 0);
+                    _rotateToObj.transform.Rotate(0,-22.5f, 0);
                 } else {
-                    _rotateToObj.transform.Rotate(0,45, 0);
+                    _rotateToObj.transform.Rotate(0,22.5f, 0);
                 }
                 _rotateTo = _rotateToObj.transform.rotation;
 
                 _inRotation = true;
 
-                SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / rotationDuration );
+                SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / RotationDuration );
                 _rotationElapsed += Time.deltaTime;
             }   
         } 
         else { //inRotation == True
-            SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / rotationDuration );
+            SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / RotationDuration );
             _rotationElapsed += Time.deltaTime;
 
-            if (_rotationElapsed >= rotationDuration){
+            if (_rotationElapsed >= RotationDuration){
                 _inRotation = false;
                 Destroy(_rotateToObj);
                 _rotationElapsed = 0.0f;
