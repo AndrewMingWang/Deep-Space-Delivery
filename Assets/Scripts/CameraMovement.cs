fb74307d4049469fd261fecb0c465e3c234bda8e
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-27 11:54:58 -0500

    Add drag panning and scroll wheel zooming

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -3,16 +3,18 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float VerticalPanSensitivity = 0.08f;
-    public float HorizontalPanSensitivity = 0.05f;
     public float RotateSensitivity = 0.05f;
+    public float ZoomSensitivity = 0.05f;
 
+    // For Panning
+    private Vector3 screenOrigin;
+    private Vector3 worldOrigin;
+
+    // For Rotation
+    private float _rotateDirection;
+    float dist;
     Vector3 cameraPos;
     Vector3 parentPos;
-
-    float dist;
-
-    private float _rotateDirection;
 
     void Start()
     {
@@ -32,86 +34,31 @@ public class CameraMovement : MonoBehaviour
 
     public void Panning()
     {
-        parentPos -= transform.right * (Input.GetAxis("Horizontal") * -1) * HorizontalPanSensitivity;
-        cameraPos -= transform.right * (Input.GetAxis("Horizontal") * -1) * HorizontalPanSensitivity;
-        parentPos -= Vector3.ProjectOnPlane(transform.up * (Input.GetAxis("Vertical") * -1),Vector3.up) * VerticalPanSensitivity;
-        cameraPos -= Vector3.ProjectOnPlane(transform.up * (Input.GetAxis("Vertical") * -1),Vector3.up) * VerticalPanSensitivity;
-        transform.parent.position = parentPos;
-        transform.position = cameraPos;
-        // transform.LookAt(transform.parent.position);
+        if (Input.GetMouseButtonDown(0))
+        {
+            screenOrigin = Input.mousePosition;
+            worldOrigin = transform.parent.position;
+        } else if (Input.GetMouseButton(0))
+        {
+            Vector3 worldDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - Camera.main.ScreenToWorldPoint(screenOrigin);
+            Vector3 planeDelta = Vector3.ProjectOnPlane(worldDelta, Vector3.up);
+
+            transform.parent.position = worldOrigin - planeDelta;
+        }
     }
 
     public void Zoom()
     {
-        if (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            Camera.main.orthographicSize = 3;
-            HorizontalPanSensitivity = 0.13f;
-            VerticalPanSensitivity = 0.26f;
-        }
-        else if (Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            Camera.main.orthographicSize = 5;
-            HorizontalPanSensitivity = 0.2f;
-            VerticalPanSensitivity = 0.4f;
-
-        }
-        else if (Input.GetKeyDown(KeyCode.Keypad3) || Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            Camera.main.orthographicSize = 8;
-            HorizontalPanSensitivity = 0.4f;
-            VerticalPanSensitivity = 0.8f;
-        }
+        Camera.main.orthographicSize -= Input.mouseScrollDelta.y * Time.deltaTime * ZoomSensitivity;
     }
 
-    // public void Rotation()
-    // {
-
-        // if (_inRotation == false)
-        // {
-        //     if (( Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.E)) && (BuildManager.BuildingSelected == false)) {
-        //         _rotateFrom = SceneObjects.transform.rotation;
-        //         _rotateToObj = new GameObject();
-        //         _rotateToObj.transform.position = new Vector3(SceneObjects.transform.position.x, SceneObjects.transform.position.y, SceneObjects.transform.position.z);
-        //         _rotateToObj.transform.rotation = new Quaternion(_rotateFrom.x, _rotateFrom.y, _rotateFrom.z, _rotateFrom.w);
-        //         if (Input.GetKeyDown(KeyCode.Q)){
-        //             _rotateToObj.transform.Rotate(0,-22.5f, 0);
-        //         } else {
-        //             _rotateToObj.transform.Rotate(0,22.5f, 0);
-        //         }
-        //         _rotateTo = _rotateToObj.transform.rotation;
-
-        //         _inRotation = true;
-
-        //         SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / RotationDuration );
-        //         _rotationElapsed += Time.deltaTime;
-        //     }   
-        // } 
-        // else { //inRotation == True
-        //     SceneObjects.transform.rotation = Quaternion.Slerp(_rotateFrom, _rotateTo, _rotationElapsed / RotationDuration );
-        //     _rotationElapsed += Time.deltaTime;
-
-        //     if (_rotationElapsed >= RotationDuration){
-        //         _inRotation = false;
-        //         Destroy(_rotateToObj);
-        //         _rotationElapsed = 0.0f;
-        //     }
-            
-            
-        // }
-
-    // }
-
     public void Rotation(){
-
+        
         Vector3 curDir = transform.localPosition.normalized * dist;
         curDir.y = 9.15f;
         transform.localPosition = curDir;
 
-       
-
         if (( Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.E)) && (BuildManager.BuildingSelected == false)) {
-                
             if (Input.GetKey(KeyCode.Q)){
                 _rotateDirection = 1f;
             } else {
