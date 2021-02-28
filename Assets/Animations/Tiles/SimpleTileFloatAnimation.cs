6c2aaa641b02c3ec1c76da6e9e5844786e2d0c4c
AndrewMingWang <andrew.wang1998@gmail.com>
2021-02-27 20:44:52 -0500

    Add float idle animation to tiles, update start gate indicator

---
+++ Assets/Animations/Tiles/SimpleTileFloatAnimation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleTileFloatAnimation : MonoBehaviour
+{
+    public Vector2 MaxYRange;
+    [HideInInspector]
+    public float MaxY;
+    public float Period;
+
+    private float t;
+    const float TWOPI = 2 * Mathf.PI;
+
+    private void Start()
+    {
+        MaxY = Random.Range(MaxYRange.x, MaxYRange.y);
+        t = Random.Range(0, TWOPI);
+        transform.position += Vector3.up * MaxY * Mathf.Sin(t * TWOPI / Period);
+    }
+
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        Vector3 offset = Vector3.up * MaxY * Mathf.Sin(t * TWOPI / Period) * Time.deltaTime;
+
+        transform.position += offset;
+
+        Tile tile = GetComponent<Tile>();
+        if (tile != null)
+        {
+            if (tile.OccupyingBuilding != null)
+            {
+                tile.OccupyingBuilding.transform.position += offset;
+            }
+        }
+    }
+}
