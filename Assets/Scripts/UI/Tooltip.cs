ee45305e79f790c197ece1d9904cdb1608d29f67
AndrewMingWang <andrew.wang1998@gmail.com>
2021-03-11 23:30:00 -0500

    Add tooltip panel

--- Assets/Scripts/UI/Tooltip.cs
+++ Assets/Scripts/UI/Tooltip.cs
@@ -6,57 +6,45 @@ using UnityEngine.EventSystems;
 
 public class Tooltip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-
-    private RectTransform tooltipView;
-    private TMP_Text tooltipText;
-    private Dictionary<string, string> elementNameToDescription = new Dictionary<string, string>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        tooltipView = GameObject.FindGameObjectWithTag("tooltip").GetComponent<RectTransform>();
-        tooltipText = tooltipView.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
-        elementNameToDescription.Add("wall", "buildable tile that blocks objects");
-        elementNameToDescription.Add("arrow", "change direction of units");
-        elementNameToDescription.Add("holding", "hold units until threshold reached");
-        elementNameToDescription.Add("trampoline", "launch units up and forward");
-    }
+    public TMP_Text Text;
+    public string Description;
+    bool _isHovering;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 screenPoint = Input.mousePosition;
-        screenPoint += new Vector3(tooltipText.preferredWidth / 2.0f, 10.0f, 0.0f);
-        tooltipView.transform.position = screenPoint;
-        if (Input.GetMouseButtonDown(0))
+        if (_isHovering)
         {
-            tooltipView.gameObject.SetActive(false);
+            //Vector3 screenPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //screenPoint += new Vector3(Text.preferredWidth/2.0f, 10.0f, 0.0f);
+            //RectTransform.transform.localPosition = screenPoint;
+            if (Input.GetMouseButtonDown(0))
+            {
+                HideTooltip();
+            }
         }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        string elementName = eventData.pointerEnter.transform.GetChild(2).GetComponent<TMP_Text>().text;
-        DisplayTooltip(elementName);
+        _isHovering = true;
+        DisplayTooltip();
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        _isHovering = false;
         HideTooltip();
     }
 
-    private void DisplayTooltip(string elementName)
+    private void DisplayTooltip()
     {
-        string description = elementNameToDescription[elementName];
-        tooltipText.text = description;
-        float padding = 10.0f;
-        tooltipView.sizeDelta = new Vector2(tooltipText.preferredWidth + padding, tooltipText.preferredHeight + padding);
-        tooltipView.gameObject.SetActive(true);
+        Text.text = Description;
     }
 
     private void HideTooltip()
     {
-        tooltipView.gameObject.SetActive(false);
+        Text.text = "";
     }
 
 }
