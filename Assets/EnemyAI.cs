4d09e2aca14e549f1af5d2c4823b073db7158f8a
AndreiGrumazescu <andrei.grumazescu@mail.utoronto.ca>
2021-03-13 14:25:54 -0500

    enemyai script minor bugfix

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -38,7 +38,7 @@ public class EnemyAI : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         switch(currState){
             case State.waiting:
