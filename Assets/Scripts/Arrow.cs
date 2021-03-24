51c1d15b2e4655b2a6753220db3036d5d50d1918
AndrewMingWang <andrew.wang1998@gmail.com>
2021-03-24 16:10:15 -0400

    Signpost rework model and pickup appearance

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -6,22 +6,26 @@ public class Arrow : Building
 {
     public MeshRenderer SignRenderer;
     public MeshRenderer PoleRenderer;
+    public MeshRenderer SignTipRenderer;
 
     public override void Awake()
     {
         SignRenderer.material.SetColor("_Color", SelectedColor);
         PoleRenderer.material.SetColor("_Color", SelectedColor);
+        SignTipRenderer.material.SetColor("_Color", SelectedColor);
     }
 
     public override void setColorPickedUp()
     {
         SignRenderer.material.SetColor("_Color", SelectedColor);
         PoleRenderer.material.SetColor("_Color", SelectedColor);
+        SignTipRenderer.material.SetColor("_Color", SelectedColor);
     }
 
     public override void setColorPlaced()
     {
         SignRenderer.material.SetColor("_Color", PlacedColor);
         PoleRenderer.material.SetColor("_Color", PlacedColor);
+        SignTipRenderer.material.SetColor("_Color", PlacedColor);
     }
 }
