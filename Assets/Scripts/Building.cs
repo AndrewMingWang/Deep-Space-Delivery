fa8f5d17515cbf6b184e42080610339d9ed38bdc
AndreiGrumazescu <andrei.grumazescu@mail.utoronto.ca>
2021-03-24 23:23:19 -0400

    holding appearing where it shouldnt fix

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -9,13 +9,14 @@ public class Building : MonoBehaviour
     public Color PlacedColor;
     public string BuildingName;
     public Tile TileUnder;
-    
+    private int _updateTime;
     protected MeshRenderer meshRenderer;
 
     public virtual void Awake()
     {
         meshRenderer = GetComponent<MeshRenderer>();
         meshRenderer.material.SetColor("_Color", SelectedColor);
+        _updateTime = 0;
     }
 
     public virtual void setColorPickedUp()
@@ -37,4 +38,13 @@ public class Building : MonoBehaviour
     {
         setColorPickedUp();
     }
+
+    // private void Update() {
+    //     if (TileUnder != null && _updateTime == 5){
+    //         transform.position = TileUnder.transform.position + SpawnHeight*transform.up;
+    //         _updateTime = 0;
+    //     } else {
+    //         _updateTime++;
+    //     }
+    // }
 }
