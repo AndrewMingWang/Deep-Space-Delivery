2367d6545a2569fd7293a38fd9c67d138fd77c70
AndrewMingWang <andrew.wang1998@gmail.com>
2021-04-02 23:15:53 -0400

    Add fastforward and ruler buttons to UI
    Need to hook up ruler button to hide ruler.
    Need to hoook up fastforward button to 2x speed.
    Need to hide fastforward button during planning

--- Assets/TurnTowardsCamera.cs
+++ Assets/TurnTowardsCamera.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class TurnTowardsCamera : MonoBehaviour
 {
+    int frame = 0;
+    int moveOnFrame = 10;
+
     private void FixedUpdate()
     {
-        Vector3 target = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
-        target.y = target.y + 0;
-        transform.LookAt(target);
+        if (frame == moveOnFrame)
+        {
+            frame = 0;
+            Vector3 target = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z);
+            target.y = target.y + 0;
+            transform.LookAt(target);
+        } else
+        {
+            frame += 1;
+        }
     }
 }
