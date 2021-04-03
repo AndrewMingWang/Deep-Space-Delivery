d794dbf53ca0b8bbf5387208af9bc54bd64daaa4
AndrewMingWang <andrew.wang1998@gmail.com>
2021-04-03 16:11:28 -0400

    add SFX to enemies

---
+++ Assets/EnemySound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySound : MonoBehaviour
+{
+    public AudioSource Source;
+    public AudioClip Alert;
+    public AudioClip Collide;
+
+    private void Awake()
+    {
+        AudioManager.EnrollSFXSource(Source);
+    }
+    public void PlayAlert()
+    {
+        Source.PlayOneShot(Alert);
+    }
+
+    public void PlayCollide()
+    {
+        Source.PlayOneShot(Collide);
+    }
+}
